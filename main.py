ccc1df69c083a97dba71c092837b59ad25bbbdaa
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-01-12 17:23:05 +0000

    first workflow

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import github
+import os
 
-g = github.Github("${{secrets.GITHUB_TOKEN}}")
+
+def get_inputs(input_name: str, prefix="INPUT") -> str:
+    """
+    Get a Github actions input by name
+
+    Args:
+        input_name (str): input_name in workflow file.
+        prefix (str, optional): prefix of input variable. Defaults to 'INPUT'.
+
+    Returns:
+        str: action_input
+
+    References
+    ----------
+    [1] https://help.github.com/en/actions/automating-your-workflow-with-github-
+    actions/metadata-syntax-for-github-actions#example
+    """
+    return os.getenv(prefix + "_{}".format(input_name).upper())
+
+
+access_token = get_inputs("ACCESS_TOKEN")
+
+g = github.Github(access_token)
 print("got g")
 user = g.get_user()
 print(user.name)
