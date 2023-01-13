ce7d0f20ab507e244d6f11225853daa9d8bb9c43
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-01-13 14:28:45 +0000

    fix: iterate through releases

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
+from get_github_data import get_inputs
 from github import Github
-import os
-
-
-def get_inputs(input_name: str, prefix="INPUT") -> str:
-    """
-    Get a Github actions input by name
-
-    Args:
-        input_name (str): input_name in workflow file.
-        prefix (str, optional): prefix of input variable. Defaults to 'INPUT'.
-
-    Returns:
-        str: action_input
-
-    References
-    ----------
-    [1] https://help.github.com/en/actions/automating-your-workflow-with-github-
-    actions/metadata-syntax-for-github-actions#example
-    """
-    return os.getenv(prefix + "_{}".format(input_name).upper())
-
 
 access_token = get_inputs("ACCESS_TOKEN")
 branch = "main"
 
 g = Github(access_token)
-print("got g")
 repo = g.get_repo("joshlynchONS/gov-changelog-action")
-print(repo)
 releases = repo.get_releases()
-print(releases)
 tags = repo.get_tags()
-print(tags)
-commits = repo.get_commits(sha=branch)
-for commit in commits:
-    message = commit.commit.message.split("\n\n")
-    print(message)
+
+releases = repo.get_releases()
+regenerate_releases = [r.tag_name for r in releases]
+
+print(releases)
+print(regenerate_releases)
--- src/get_github_data.py
+++ src/get_github_data.py
@@ -1,5 +1,20 @@
-import github
+import os
 
-g = github.Github('${{secrets.GITHUB_TOKEN}}')
-user = g.get_user()
-print(user.name)
+
+def get_inputs(input_name: str, prefix="INPUT") -> str:
+    """
+    Get a Github actions input by name
+
+    Args:
+        input_name (str): input_name in workflow file.
+        prefix (str, optional): prefix of input variable. Defaults to 'INPUT'.
+
+    Returns:
+        str: action_input
+
+    References
+    ----------
+    [1] https://help.github.com/en/actions/automating-your-workflow-with-github-
+    actions/metadata-syntax-for-github-actions#example
+    """
+    return os.getenv(prefix + "_{}".format(input_name).upper())
