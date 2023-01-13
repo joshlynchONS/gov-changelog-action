4f92dc03408d00587fe8ca8f4a517afa565a18f3
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-01-13 15:08:56 +0000

    fix: iterate through tags

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ g = Github(access_token)
 repo = g.get_repo("joshlynchONS/gov-changelog-action")
 releases = repo.get_releases()
 tags = repo.get_tags()
+tags_sha = {}
+for tag in tags:
+    tags_sha[tag.name] = tag.commit.sha
 
 releases = repo.get_releases()
 regenerate_releases = [r.tag_name for r in releases]
 
 print(releases)
 print(regenerate_releases)
+print(tags)
+print(tags_sha)
