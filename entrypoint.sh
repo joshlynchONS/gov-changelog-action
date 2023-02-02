955357128c82fa98238ed548e70f9e6b7a27df70
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-02-02 11:06:12 +0000

    fix: update entrypoint to include package install

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 python -m pip install --upgrade pip setuptools wheel
 pip install -r /requirements.txt
+pip install .
 
 python /main.py /gov_changelog_action/src/get_github_data.py /gov_changelog_action/src/manip_data.py /gov_changelog_action/src/create_changelog.py /gov_changelog_action/src/template.txt /changelog-config.yml
