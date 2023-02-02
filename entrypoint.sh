0461630e9488249c3bdd4be5505b1cc9eb5c0486
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-02-02 16:43:53 +0000

    fix: add venv to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+python -m venv vpy
+. vpy/bin/activate
 python -m pip install --upgrade pip setuptools wheel
 pip install -r /requirements.txt
-python /setup.py install
+pip install -e .
 
 python /main.py /gov_changelog_action/src/get_github_data.py /gov_changelog_action/src/manip_data.py /gov_changelog_action/src/create_changelog.py /gov_changelog_action/src/template.txt /changelog-config.yml
