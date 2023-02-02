8eab0bf9eaa29ff2b72ea5869856d2553f3988df
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-02-02 15:34:20 +0000

    fix: fix entrypoint setup.py install

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 python -m pip install --upgrade pip setuptools wheel
 pip install -r /requirements.txt
-python setup.py install
+python -r /setup.py install
 
 python /main.py /gov_changelog_action/src/get_github_data.py /gov_changelog_action/src/manip_data.py /gov_changelog_action/src/create_changelog.py /gov_changelog_action/src/template.txt /changelog-config.yml
